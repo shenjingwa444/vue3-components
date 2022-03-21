f2c415db33278aa15d9dc744c11386e50db0ca2d
jasoncheng <shenjingwa444@qq.com>
2022-03-21 21:08:30 +0800

    完成 Home.vue 页面样式

--- pixu-ui/src/view/Home.vue
+++ pixu-ui/src/view/Home.vue
@@ -5,17 +5,39 @@
     <h2>一个简易的 UI 框架</h2>
     <p class="actions">
       <a href="">Github</a>
-      <a href="">开始</a>
+      <router-link to="/Doc">开始</router-link>
     </p>
   </div>
 </template>
 
-<style lang="scss" scoped>
 
-</style>
 <script>
 import Topnav from "../components/Topnav.vue"
 export default {
   components: {Topnav}
 }
 </script>
+
+<style lang="scss" scoped>
+.banner{
+  padding:100px 0;
+  display:flex;
+  justify-content: center;
+  align-items:center;
+  flex-direction:column;
+  background-color: lightgreen;
+  > .actions{
+    padding:8px 0;
+    a{
+      margin:0 8px;
+      background-color: #fff;
+      display: inline-block;
+      $h:28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding:0 8px;
+    }
+  }
+}
+</style>
