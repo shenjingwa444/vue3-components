c203e70b178b21207710ff3d6062915b65a82d7c
jasoncheng <shenjingwa444@qq.com>
2022-03-22 02:58:00 +0800

    完成 CSS 整体布局

--- pixu-ui/src/main.ts
+++ pixu-ui/src/main.ts
@@ -1,17 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
-import {createWebHashHistory,createRouter} from "vue-router"
-import Home from './view/Home.vue'
-import Doc from './view/Doc.vue'
-const history = createWebHashHistory()
-const router = createRouter({
-  history:history,
-  routes:[
-    {path:'/',component:Home},
-    {path:'/doc',component:Doc}
-  ]
-})
+import {router} from './router';
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
