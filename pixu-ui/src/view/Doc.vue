f2ed188027ece90b73eb0daffc0347fa7e78edfa
jasoncheng <shenjingwa444@qq.com>
2022-03-22 01:09:35 +0800

    实现 Doc.vue 的 aside 切换功能

--- pixu-ui/src/view/Doc.vue
+++ pixu-ui/src/view/Doc.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <Topnav/>
-    <div class="content">
-      <aside>
+    <div class="content" >
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -23,12 +23,19 @@
     </div>
   </div>
 </template>
-<script>
+
+<script lang="ts">
 import Topnav from "../components/Topnav.vue"
+import {inject,Ref} from 'vue';
 export default {
-  components: {Topnav}
+  components: {Topnav},
+  setup(){
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
+    return {menuVisible}
+  }
 }
 </script>
+
 <style lang="scss" scoped>
 aside{
   background-color: lightblue;
