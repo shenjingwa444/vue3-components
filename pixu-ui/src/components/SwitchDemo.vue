d758e04ee5d5c26c96371e6d094a3255db558531
jasoncheng <shenjingwa444@qq.com>
2022-03-22 03:44:25 +0800

    初步完成 Switch 页面CSS，按钮左右滑动功能

--- pixu-ui/src/components/SwitchDemo.vue
+++ pixu-ui/src/components/SwitchDemo.vue
@@ -1,34 +1,14 @@
 <template>
   <div>
-    这是 Switch 的文档,    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-    这是 Switch 的文档,
-
+    <Switch />
   </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+components:{
+  Switch,
+}
+}
+</script>
