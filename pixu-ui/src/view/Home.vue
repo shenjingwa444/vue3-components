2cb9a70d7e094a2fcb8b14358b0e39e5fc461032
jasoncheng <shenjingwa444@qq.com>
2022-03-21 20:57:08 +0800

    封装 Topnav 组件

---
+++ pixu-ui/src/view/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Topnav/>
+  <div class="banner">
+    <h1>貔貅UI</h1>
+    <h2>一个简易的 UI 框架</h2>
+    <p class="actions">
+      <a href="">Github</a>
+      <a href="">开始</a>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+<script>
+import Topnav from "../components/Topnav.vue"
+export default {
+  components: {Topnav}
+}
+</script>
