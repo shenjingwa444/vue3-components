e60a77f59f2ea4da825ab3383ecd6b825673846a
jasoncheng <shenjingwa444@qq.com>
2022-03-22 11:28:45 +0800

    Switch.vue 接受 props ，改变 checked状态

--- pixu-ui/src/lib/Switch.vue
+++ pixu-ui/src/lib/Switch.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
-    <button><span></span></button>
+    <button :class="{checked:value}" @click="toggle">
+      <span></span>
+    </button>
   </div>
 </template>
 
+<script lang="ts">
+import {ref} from 'vue';
+
+export default {
+  props:{
+    value:Boolean,
+  },
+  setup(props,context) {
+    const toggle = () => {
+      context.emit('input',!props.value)
+    };
+    return {toggle};
+  }
+};
+</script>
+
 <style lang="scss">
-$h:22px;
-$h2:$h - 4px;
-button{
+$h: 22px;
+$h2: $h - 4px;
+button {
   height: $h;
   width: $h*2;
-  border:none;
-  background: blue;
-  border-radius:$h/2;
-  position:relative;
+  background: gray;
+  border: none;
+  border-radius: $h/2;
+  position: relative;
 }
-span{
-  position:absolute;
-  top:2px;
-  left:2px;
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
   height: $h2;
   width: $h2;
-  background:white;
-  border-radius:$h2/2;
+  background: white;
+  border-radius: $h2/2;
+  transition: left 1s;
 }
-button:hover > span{
-  left:calc(100% - #{$h2} - 2px)
+
+button.checked {
+  background: blue;
+
+}
+
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px)
+}
+button:focus{
+  outline:none;
 }
 </style>
