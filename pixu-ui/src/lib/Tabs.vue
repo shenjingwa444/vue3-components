2f0a4d6430248bbadf8d9e16da6ad5c99fdaf593
jasoncheng <shenjingwa444@qq.com>
2022-03-23 22:04:57 +0800

    动态设置 div 的宽度

--- pixu-ui/src/lib/Tabs.vue
+++ pixu-ui/src/lib/Tabs.vue
@@ -1,70 +1,97 @@
 <template>
   <div class="pixu-tabs">
     <div class="pixu-tabs-nav">
-      <div :class="{selected:t===selected}" @click="select(t)" class="pixu-tabs-nav-item" v-for="(t,index) in titles" :key='index'>{{ t }}</div>
+      <div :class="{selected:t===selected}"
+           @click="select(t)" class="pixu-tabs-nav-item"
+           v-for="(t,index) in titles" :key='index'
+           :ref="(el) => { if(el) navItems[index] = el }">{{ t }}</div>
+      <div class="pixu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="pixu-tabs-content">
       <component :is="current" :key="current.props.title" class="pixu-tabs-content-item"/>
-      {{current.props.title}}
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {computed} from 'vue';
+import {computed, onMounted, ref} from 'vue';
 
 export default {
-  props:{
-    selected:{
-      type:String,
+  props: {
+    selected: {
+      type: String,
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null)
+    onMounted(() => {
+      const divs = navItems.value;
+      const result = divs.filter((item)=>{
+        return item.classList.contains('selected')
+      })[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+    });
     const defaults = context.slots.default();
     const titles = defaults.map(tag => tag.props.title);
-    const current = computed(()=>{
-      return defaults.filter((tag)=>{
-        return tag.props.title === props.selected
-      })[0]
-    })
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 字标签必须是 Tab');
       }
     });
-    const select = (title)=>{
-      context.emit('update:selected',title)
-    }
-    return {defaults, titles,current,select};
+    const select = (title) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select, navItems,indicator};
   }
 };
 </script>
 
 <style lang="scss">
-$blue:#40a9ff;
-$color:#333;
-$border-color:#d9d9d9;
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
 
-.pixu-tabs{
-  &-nav{
+.pixu-tabs {
+  &-nav {
     display: flex;
-    color:$color;
-    border-bottom:1px solid $border-color;
-    &-item{
-      padding:8px 0;
-      margin:0 16px;
-      cursor:pointer;
-      &:first-child{
-        margin-left:0;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
       }
-      &.selected{
-        color:$blue;
+
+      &.selected {
+        color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left:0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
-  &-content{
-    padding:8px 0;
+
+  &-content {
+    padding: 8px 0;
   }
 }
 </style>
