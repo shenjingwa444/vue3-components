a79a7b71c0199f0156753302e3cc8e436b312700
jasoncheng <shenjingwa444@qq.com>
2022-03-23 12:49:11 +0800

    完成 Tabs CSS

--- pixu-ui/src/lib/Tabs.vue
+++ pixu-ui/src/lib/Tabs.vue
@@ -1,22 +1,55 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key='index'>{{t}}</div>
-    <component v-for="(c,index) in defaults" :key="index" :is="c"/>
+  <div class="pixu-tabs">
+    <div class="pixu-tabs-nav">
+      <div class="pixu-tabs-nav-item" v-for="(t,index) in titles" :key='index'>{{ t }}</div>
+    </div>
+    <div class="pixu-tabs-content">
+      <component class="pixu-tabs-content-item" v-for="(c,index) in defaults" :key="index" :is="c"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+
 export default {
-  setup(props,context){
-    const defaults = context.slots.default()
-    const titles = defaults.map(tag=>tag.props.title)
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
-        throw new Error('Tabs 字标签必须是 Tab')
+  setup(props, context) {
+    const defaults = context.slots.default();
+    const titles = defaults.map(tag => tag.props.title);
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 字标签必须是 Tab');
       }
-    })
-    return {defaults,titles}
+    });
+    return {defaults, titles};
+  }
+};
+</script>
+
+<style lang="scss">
+$blue:#40a9ff;
+$color:#333;
+$border-color:#d9d9d9;
+
+.pixu-tabs{
+  &-nav{
+    display: flex;
+    color:$color;
+    border-bottom:1px solid $border-color;
+    &-item{
+      padding:8px 0;
+      margin:0 16px;
+      cursor:pointer;
+      &:first-child{
+        margin-left:0;
+      }
+      &.selected{
+        color:$blue;
+      }
+    }
+  }
+  &-content{
+    padding:8px 0;
   }
 }
-</script>
+</style>
