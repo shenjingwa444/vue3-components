3a219108be64e04f63dff5f137860af8749a2cd3
jasoncheng <shenjingwa444@qq.com>
2022-03-24 12:00:34 +0800

    添加 svg ，实现圆弧

--- pixu-ui/src/components/Topnav.vue
+++ pixu-ui/src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav" >
-    <div class="logo" >LOGO</div>
+    <div class="logo" >
+      <svg class="icon">
+        <use xlink:href="#icon-pixiu"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -24,7 +28,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color:#048c86;
 .topnav {
+  color:$color;
   display: flex;
   padding: 16px;
   position:fixed;
@@ -37,6 +43,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 32px;
+      height: 32px;
+    }
   }
 
   > .menu {
--- pixu-ui/src/view/Home.vue
+++ pixu-ui/src/view/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <Topnav/>
-  <div class="banner">
-    <h1>貔貅UI</h1>
-    <h2>一个简易的 UI 框架</h2>
-    <p class="actions">
-      <a href="">Github</a>
-      <router-link to="/Doc">开始</router-link>
-    </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>貔貅UI</h1>
+        <h2>一个简易的 UI 框架</h2>
+        <p class="actions">
+          <a href="">Github</a>
+          <router-link to="/Doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-dengpao"></use>
+      </svg>
+    </div>
   </div>
 </template>
-
 
 <script>
 import Topnav from "../components/Topnav.vue"
@@ -20,27 +34,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #048c86;
+$green: #02bcb0;
+$border-radius: 4px;
+.topnavAndBanner {
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
+
+.features {
+  > svg {
+    height: 64px;
+    width: 64px;
+  }
+}
+
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
 
   > .actions {
     padding: 8px 0;
 
     a {
       margin: 0 8px;
-      background-color: #fff;
+      background-color: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
+
 </style>
